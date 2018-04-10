bd4a023cc37f8617a3c6534aa4ca29268c2e4a0c
Conley <cernst3@u.rochester.edu>
2018-04-10 13:40:18 -0400

    image added

--- cernst3/content.php
+++ cernst3/content.php
@@ -48,10 +48,12 @@
         <!-- Right Col -->
         <div class="col-4">
           <div class="card">
+            <!-- <img class="card-img" src="imgs/girl.jpg" alt="Card image"> -->
             <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <img class="card-img" src="imgs/girl.jpg" alt="Card image">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
           </div>
           <div class="card">
