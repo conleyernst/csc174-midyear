f6020630f7b69c407187c818f53a8424bc7fe871
Conley <cernst3@u.rochester.edu>
2018-04-10 07:59:44 -0400

    reworking of files

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang = "en">
+  <head>
+
+      <meta charset= "utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Assignment 4- Lagos by Fiyinfoluwa Oluyinka, Olatunde Oshunlaja, Prosper Feya </title>
+
+</head>
+
+<body>
+
+  <?php include "includes/newnav.php"; ?>
+
+
+  <div class="tri-container">
+    <section class="col">
+    <div class="slider"> <!-- JavaSCRIPT Slider -->
+
+        <!-- In this slider, the IA chose images that represent the African continent, ensuring that attractions from Nigeria, Zimbabwe (The countries where Ola, Fiyin and Prosper are from), as well as Safaris from Kenya and South Africa, as these are strong representations of the African continent -->
+
+          <img src="images/bp1.png" alt = "Victoria Falls Zimbabwe" />
+          <img src="images/bp2.jpg" alt = "Zuma Rock in Nigeria" />
+          <img src="images/bp3.png" alt = "Safari Animals Kenya" />
+    </div>
+    </section>
+    <section class="col">
+    <div class="slider"> <!-- JavaSCRIPT Slider -->
+
+        <!-- In this slider, the IA chose images that represent the African continent, ensuring that attractions from Nigeria, Zimbabwe (The countries where Ola, Fiyin and Prosper are from), as well as Safaris from Kenya and South Africa, as these are strong representations of the African continent -->
+
+          <img src="images/b1.jpg" alt = "Victoria Falls Zimbabwe" />
+          <img src="images/b2.jpg" alt = "Zuma Rock in Nigeria" />
+          <img src="images/b3.jpg" alt = "Safari Animals Kenya" />
+    </div>
+    </section>
+    <section class="col">
+    <div class="slider"> <!-- JavaSCRIPT Slider -->
+
+        <!-- In this slider, the IA chose images that represent the African continent, ensuring that attractions from Nigeria, Zimbabwe (The countries where Ola, Fiyin and Prosper are from), as well as Safaris from Kenya and South Africa, as these are strong representations of the African continent -->
+
+          <img src="images/nb1.jpg" alt = "Victoria Falls Zimbabwe" />
+          <img src="images/nb2.jpg" alt = "Zuma Rock in Nigeria" />
+          <img src="images/nb3.jpg" alt = "Safari Animals Kenya" />
+    </div>
+    </section>
+  </div>
+
+  <div class = "container">
+    <section class="col home-content">
+      <p>foo bar.</p>
+    </section>
+    <section class="col home-content">
+<!--       <img src="images/sarah.jpg" alt="Sarah Ogunji">
+ -->      <div class="home-content">
+        <h2 class="accent-color"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge accent-color"></i><a href="sarah.php">Meet Sarah</a></h2>
+          <p>Sarah brings a Digital Media Background to a technical team!</p>
+      </div>
+    </section>
+    <section class="col home-content">
+      <a href="submit.php" class="button">Connect With Sarah!</a>
+    </section>
+
+  </div>
+      <!-- FORM HERE -->
+    </aside>
+  </section>
+
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script src="js/menu-highlighter.js"></script>
+
+  <!-- ******************************** -->
+
+  <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+  <script type = "text/javascript" src="sss/sss.min.js"></script>
+  <script type = "text/javascript">
+
+    $( "#accordion" ).accordion({
+      heightStyle: "content"
+    });
+    $('.slider').sss({
+      slideShow : true, 
+    });
+
+  </script>
+</div>
+
+</body>
+
+<!-- Further Notes from the IA -->
+
+<!-- As the IA, I have already ensured that the overall content follows a similar pattern. There is an "About Me" column on each page, giving a brief overview of the student. Next we have Work Experience, and then Extra-Curricular Activities. A grid layout, although it might hamper the readability a little bit, considering the copious amounts of words, it prevents a long scrolly page-->
+
+<!-- Also the IA ensured that all the pictures have a common size and theme. They all have a max height of 250, and more importantly, each Student is wearing African attire, thus tying in with the overall theme and ambience of the website. -->
+</html>
+
+
---
+++ prosper.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Prosper</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<body>
+
+  <?php include "includes/newnav.php"; ?>
+
+
+
+
+<div class="container-unequal">
+  <div class="col">
+    <p>foobar</p>
+  </div>
+  <div class="col">
+    <p>foobar</p>
+  </div>
+</div>
+
+</body>
+</html>
